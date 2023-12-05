4edc2740ed790118a90e64e2cfd059c575cc6a3e
Matvey <syritsamatvey10@gmail.com>
2023-12-06 00:02:58 +0300

    Added Lab_3

--- Lab_3/Librarian.cpp
+++ Lab_3/Librarian.cpp
@@ -1,17 +1,20 @@
 #include "librarian.h"
 #include <iostream>
+#include<stdexcept>
 
 Librarian::Librarian(const std::string& firstName, const std::string& lastName, int age)
     : User(firstName, lastName, age) {}
 
 void Librarian::searchBookOnShelf(Shelf& shelf, const std::string& title, User& user) const {
     std::cout << "Librarian " << getFirstName() << " " << getLastName() << " is searching for the book \"" << title << "\"...\n";
-    if (shelf.searchBook(title)) {
-        std::cout << "Librarian " << getFirstName() << " " << getLastName() << " is giving the book \"" << title << "\" to User " << user.getFirstName() << " " << user.getLastName() << ".\n";
-        shelf.removeBook(title);
+    try {
+        if (shelf.searchBook(title)) {
+            std::cout << "Librarian " << getFirstName() << " " << getLastName() << " is giving the book \"" << title << "\" to User " << user.getFirstName() << " " << user.getLastName() << ".\n";
+            shelf.removeBook(title);
+        }
     }
-    else {
-        std::cout << "Sorry, we don't have a book with this title " << "\n";
+    catch (const std::out_of_range& ex) {
+        std::cerr << "Exception caught: " << ex.what() << "\n";
     }
 }
 
